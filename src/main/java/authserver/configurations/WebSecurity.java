f3896c88ce2e6c8b808990f34ff20520e115c33a
Ketan R <ketan_r598@outlook.com>
2024-07-25 18:40:49 +0530

    added authManager bean and login filter

--- src/main/java/authserver/configurations/WebSecurity.java
+++ src/main/java/authserver/configurations/WebSecurity.java
@@ -1,20 +1,35 @@
 package authserver.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import authserver.filters.LoginFilter;
+import authserver.services.KeysProvider;
 import authserver.services.UserAuthenticationDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
+	
+	@Autowired
+	private KeysProvider keyProvider;
+	
 
+    @Bean
+    public LoginFilter loginFilter(AuthenticationManager authManager) {
+        return new LoginFilter(authManager, keyProvider);
+    }
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
@@ -29,7 +44,12 @@ public class WebSecurity {
 		http.cors(cors -> cors.disable());
 		http.csrf(csrf -> csrf.disable());
 		
+//		AuthenticationManager authManager = http.getSharedObject(AuthenticationManagerBuilder.class)
+//				                                .getOrBuild();
+		http.addFilterBefore(loginFilter(authManager(http)), BasicAuthenticationFilter.class);
+		
 		return http.build();
+			
 	}
 	
 	@Bean
@@ -41,4 +61,11 @@ public class WebSecurity {
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authManagerBuilder.userDetailsService(userDetailsService()).passwordEncoder(encoder());
+        return authManagerBuilder.build();
+    }
 }
